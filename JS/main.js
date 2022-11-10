252507658c5841aa0a8792ec36d912349cb2201c
FilippoBid <filippoperrone1997@gmail.com>
2022-11-10 20:16:07 +0100

    fine fix esercizio

--- JS/main.js
+++ JS/main.js
@@ -42,8 +42,8 @@ startBtn.addEventListener("click", function () {
  * @param {num} max 
  * @returns {num}
  */
-function getRndNS0(max) {
-    return Math.floor(Math.random() * (max - 0 + 1)) + 0;
+function getRndNS1(max) {
+    return Math.floor(Math.random() * (max - 1 + 1)) + 1;
 }
 
 
@@ -83,20 +83,17 @@ let NSquares = Math.pow(NCol, 2);
  * @param {number} containerSquare 
  * @return {Element}  
  */
-
 function creaGrid(selCol, containerSquare) {
     containerEl.innerHTML = ""
-
-
     if (selCol == "1") {
-
         NCol = 10;
+        NSquares = Math.pow(NCol, 2);
     } else if (selCol === "2") {
-
         NCol = 9;
+        NSquares = Math.pow(NCol, 2);
     } else if (selCol === "3") {
-
         NCol = 7;
+        NSquares = Math.pow(NCol, 2);
     }
 
 
@@ -108,13 +105,26 @@ function creaGrid(selCol, containerSquare) {
         square.classList.add("squarestyle");
         square.classList.add("text-center");
         square.classList.add("vertical-align");
+        bombs = getRandom16Array(NSquares);
+
+        if((bombs.includes(i+1)) === true){
+            square.dataset.boom = "1"
+                
+        }
+
         square.addEventListener("click", function () {
-            this.classList.toggle("bg-danger")
+
+            if (this.dataset.boom === "1"){
+                alert("hai perso")
+            };
+        
+            this.classList.toggle("bg-danger");
+            
             console.log(square.innerHTML);
         })
 
-        square.style.width = `calc(100% / ${NCol})`
-        square.innerHTML = `${i + 1}`
+        square.style.width = `calc(100% / ${NCol})`;
+        square.innerHTML = `${i + 1}`;
         containerSquare.append(square);
     }
 
@@ -123,31 +133,30 @@ function creaGrid(selCol, containerSquare) {
 
 
 
-}
+ }
 
 
 
 
-startBtn.addEventListener("click", function () {
-    const selectD = selectDEl.value;
-    creaGrid(selectD, containerEl);
-    let bombs = getRandom16Array(NSquares);
-
+ startBtn.addEventListener("click", function () {
+     const selectD = selectDEl.value;
+     creaGrid(selectD, containerEl);
+     
 });
 
 
 /**
  * 
  * @param {number} maxRandomN ;
- * @return {Array}  ;
+ * @return {Array};
  */
 
 function getRandom16Array (maxRandomN){
 
     let bombListArray = [];
     
-    while(bombListArray.length >16 ) {
-        let bombN = getRndNS0(maxRandomN);
+    while(bombListArray.length < 16 ) {
+        let bombN = getRndNS1(maxRandomN);
         if(!bombListArray.includes(bombN)){bombListArray.push(bombN)};
 
     } return bombListArray
